0662346d960f9c7bd9a9d2d7b008e741ea888548
Fork TIPS <jojapoppa@protonmail.com>
2018-11-06 22:57:18 +0000

    remove reward error msg...

--- src/CryptoNoteCore/Blockchain.cpp
+++ src/CryptoNoteCore/Blockchain.cpp
@@ -858,11 +858,14 @@ bool Blockchain::validate_miner_transaction(const Block& b, uint32_t height, siz
     logger(ERROR, BRIGHT_RED) << "Coinbase transaction spend too much money: " << m_currency.formatAmount(minerReward) <<
       ", block reward is " << m_currency.formatAmount(reward);
     return false;
-  } else if (minerReward < reward) {
-    logger(ERROR, BRIGHT_RED) << "Coinbase transaction doesn't use full amount of block reward: spent " <<
-      m_currency.formatAmount(minerReward) << ", block reward is " << m_currency.formatAmount(reward);
-    return false;
   }
+
+  // jojapoppa 
+  //else if (minerReward < reward) {
+  //  logger(ERROR, BRIGHT_RED) << "Coinbase transaction doesn't use full amount of block reward: spent " <<
+  //    m_currency.formatAmount(minerReward) << ", block reward is " << m_currency.formatAmount(reward);
+  //  return false;
+  // }
 
   return true;
 }
