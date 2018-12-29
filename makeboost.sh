e79c9bf8041817b9acd868e11ca7ad0dc5b09883
Rob Wynden <rob.wynden@ucsf.edu>
2018-12-28 21:06:01 -0800

    update build on mac

--- makeboost.sh
+++ makeboost.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 # First, from home folder do...
+# note: but first on mac ONLY... ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" ... then brew install wget --with-libressl ... then rename the download file to boost_1_65_0.tar.gz
 # wget "https://sourceforge.net/projects/boost/files/boost/1.65.0/boost_1_65_0.tar.gz/download"
 # then... tar -xvzf boost_1_65_0.tar.gz
 
@@ -8,6 +9,6 @@ cd ~/boost_1_65_0
 export BOOST_HAS_THREADS ON
 export BOOST_HAS_PTHREADS ON
 export Boost_USE_MULTITHREADED ON
-./bootstrap.sh --prefix=/home/fork/boostfedora 
-./b2 install --prefix=/home/fork/boostfedora --layout=tagged --threading=multi --without-mpi --build-type=complete
+./bootstrap.sh --prefix=~/boostfedora 
+./b2 install --prefix=~/boostfedora --layout=tagged --threading=multi --without-mpi --build-type=complete
 cd ~
