86bbf955126ce6687d42bd46b815531266a3287b
Rob Wynden <rob.wynden@ucsf.edu>
2018-12-29 07:52:52 -0800

    more changes for mac build option

--- makeboost.sh
+++ makeboost.sh
@@ -1,14 +1,18 @@
 #!/bin/sh
 
 # First, from home folder do...
-# note: but first on mac ONLY... ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" ... then brew install wget --with-libressl ... then rename the download file to boost_1_65_0.tar.gz
+# note: but first on mac ONLY... ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" ... then brew install wget --with-libressl ... then rename the download file to boost_1_65_0.tar.gz and unzip that in your home folder
 # wget "https://sourceforge.net/projects/boost/files/boost/1.65.0/boost_1_65_0.tar.gz/download"
 # then... tar -xvzf boost_1_65_0.tar.gz
+# just put boost source 1 folder up
 
-cd ~/boost_1_65_0
+cd ../boost_1_65_0
 export BOOST_HAS_THREADS ON
 export BOOST_HAS_PTHREADS ON
 export Boost_USE_MULTITHREADED ON
-./bootstrap.sh --prefix=~/boostfedora 
-./b2 install --prefix=~/boostfedora --layout=tagged --threading=multi --without-mpi --build-type=complete
-cd ~
+
+# using full path here... just edit on each platform...
+./bootstrap.sh --prefix=/home/fork/boostfedora 
+./b2 install --prefix=/home/fork/boostfedora --layout=tagged --threading=multi --without-mpi --build-type=complete
+cd ..
+ls
