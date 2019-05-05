9bcc390022b859a1c1c45463e1a78284be1c6cd1
Fork TIPS <jojapoppa@protonmail.com>
2019-05-05 01:19:42 +0000

    fixing static link on linux...

--- makefedora.sh
+++ makefedora.sh
@@ -23,4 +23,4 @@ export BOOST_ROOT=/home/fork/fedoragold-release/boostfedora
 # cmake -G "Visual Studio 15 2017 Win64" ..
 # cmake --build . --config Release
 
-make build-release
+make build-release 
