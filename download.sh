4fd172846e1e2c659b9c9f84e14fab4ba0775583
jojapoppa <jojapoppa@protonmail.com>
2019-10-31 22:40:55 -0700

    adding bootstrap script to repo...

---
+++ download.sh
@@ -0,0 +1,6 @@
+wget 5.189.155.88/FEDbu/blockchainindices.dat
+wget 5.189.155.88/FEDbu/blockindexes.dat
+wget 5.189.155.88/FEDbu/blockscache.dat
+wget 5.189.155.88/FEDbu/blocks.dat
+wget 5.189.155.88/FEDbu/poolstate.bin
+
