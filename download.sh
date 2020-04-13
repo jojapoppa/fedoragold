a1ade59218ff9eeca346e5e01d5e1db5e519463d
jojapoppa <jojapoppa@protonmail.com>
2020-04-13 11:46:03 -0700

    removed poolstate.bin from bootstrap

--- download.sh
+++ download.sh
@@ -2,5 +2,4 @@ wget 5.189.155.88/FEDbu/blockchainindices.dat
 wget 5.189.155.88/FEDbu/blockindexes.dat
 wget 5.189.155.88/FEDbu/blockscache.dat
 wget 5.189.155.88/FEDbu/blocks.dat
-wget 5.189.155.88/FEDbu/poolstate.bin
-
+# wget 5.189.155.88/FEDbu/poolstate.bin
