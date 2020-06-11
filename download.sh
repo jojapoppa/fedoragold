b6a51f0feb25472b5190eeb157ed285e0ed758ce
Joja Poppa <jojapoppa@protonmail.com>
2020-06-11 13:58:15 -0700

    added update for bootstrap instructions...

--- download.sh
+++ download.sh
@@ -1,5 +1,5 @@
-wget 5.189.155.88/FEDbu/blockchainindices.dat
-wget 5.189.155.88/FEDbu/blockindexes.dat
-wget 5.189.155.88/FEDbu/blockscache.dat
-wget 5.189.155.88/FEDbu/blocks.dat
-# wget 5.189.155.88/FEDbu/poolstate.bin
+wget --no-check-certifiate 5.189.155.88/FEDbu/blockchainindices.dat
+wget --no-check-certifiate 5.189.155.88/FEDbu/blockindexes.dat
+wget --no-check-certifiate 5.189.155.88/FEDbu/blockscache.dat
+wget --no-check-certifiate 5.189.155.88/FEDbu/blocks.dat
+# --no-check-certifiate wget 5.189.155.88/FEDbu/poolstate.bin
