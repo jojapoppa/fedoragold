b1223d6ece1f734594402638b7f43b13dd87ffc1
jojapoppa <jojapoppa@protonmail.com>
2020-07-25 06:37:08 -0700

    arm...

--- src/Platform/Linux/crypto/slow-hash.c
+++ src/Platform/Linux/crypto/slow-hash.c
@@ -12,6 +12,23 @@
 #else
 #include <sys/mman.h>
 #include <arm_neon.h>
+#pragma once
+#include "TargetConditionals.h"
+#if TARGET_OS_SIMULATOR
+#define __cpuid_count(__level, __count, __eax, __ebx, __ecx, __edx) \
+    __asm(" xchgq %%rbx,%q1\n" \
+          " cpuid\n" \
+          " xchgq %%rbx,%q1" \
+        : "=a"(__eax), "=r" (__ebx), "=c"(__ecx), "=d"(__edx) \
+        : "0"(__level), "2"(__count))
+#else
+static inline __attribute__((always_inline))
+void __cpuid_count(uint32_t __level, int32_t __count,
+                   int32_t &__eax, int32_t &__ebx, int32_t &__ecx, int32_t &__edx)
+{
+    __eax = __ebx = __ecx = __edx = -1;
+}
+#endif
 typedef uint8x16_t __m128i;
 inline __m128i _mm_shuffle_epi32 (__m128i a, int imm)
 {
