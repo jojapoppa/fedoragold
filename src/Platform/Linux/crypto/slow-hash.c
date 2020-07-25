fb2ce62a1e28955dc7f15f4ad2e0339e67dfdf9a
jojapoppa <jojapoppa@protonmail.com>
2020-07-24 19:11:58 -0700

    arm...

--- src/Platform/Linux/crypto/slow-hash.c
+++ src/Platform/Linux/crypto/slow-hash.c
@@ -15,12 +15,11 @@ typedef uint8x16_t __m128i;
 inline __m128i _mm_shuffle_epi32 (__m128i a, int imm)
 {
   switch (imm) {
-      case 0 :
+      case 0:
         return vdupq_n_s32(vgetq_lane_s32(a, 0)); 
-        break;
+        //break;
       default: 
-        //__m128i ret;
-        uint8x16_t ret;
+        __m128i ret;
         ret[0] = a[imm & 0x3];
         ret[1] = a[(imm >> 2) & 0x3];
         ret[2] = a[(imm >> 4) & 0x03];
