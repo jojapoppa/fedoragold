35da5038c0ba4db96fc3487cdf08b6121f7195be
jojapoppa <jojapoppa@protonmail.com>
2020-07-24 20:04:26 -0700

    arm...

--- src/Platform/Linux/crypto/slow-hash.c
+++ src/Platform/Linux/crypto/slow-hash.c
@@ -25,21 +25,17 @@ inline __m128i _mm_shuffle_epi32 (__m128i a, int imm)
   ret[3] = a[(imm >> 6) & 0x03];
   return ret;
 }
-// Shifts the 128-bit value in a left by imm bytes while shifting in zeros. imm must be an immediate.
-//FORCE_INLINE __m128i _mm_slli_si128(__m128i a, __constrange(0,255) int imm)
-#define _mm_slli_si128(a, imm) \
+inline __m128i _mm_setzero_si128()
+{
+	return vreinterpretq_m128i_s32(vdupq_n_s32(0));
+}
+#define _mm_slli_si128(a1, im) \
 ({ \
-  __m128i ret; \
-  if ((imm) <= 0) { \
-    ret = a; \
-  } \
-  else if ((imm) > 15) { \
-    ret = _mm_setzero_si128(); \
-  } \
-  else { \
-    ret = vreinterpretq_m128i_s8(vextq_s8(vdupq_n_s8(0), vreinterpretq_s8_m128i(a), 16 - (imm))); \
-  } \
-  ret; \
+  __m128i retn; \
+  if (im <= 0) { retn = a1; } \
+  else if (im > 15) { retn = _mm_setzero_si128(); } \
+  else { retn = vreinterpretq_m128i_s8(vextq_s8(vdupq_n_s8(0), vreinterpretq_s8_m128i(a1), 16 - (im))); } \
+  retn; \
 })
 #endif
 
