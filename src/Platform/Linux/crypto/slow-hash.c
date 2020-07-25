9a3eff47cef879302d1dd9551304c0bdbf8d16f7
jojapoppa <jojapoppa@protonmail.com>
2020-07-24 22:01:33 -0700

    arm...

--- src/Platform/Linux/crypto/slow-hash.c
+++ src/Platform/Linux/crypto/slow-hash.c
@@ -29,7 +29,7 @@ inline __m128i _mm_shuffle_epi32 (__m128i a, int imm)
 #define vreinterpretq_s8_m128i(x) vreinterpretq_s8_s32(x)
 #define vreinterpretq_m128i_s8(x) vreinterpretq_s32_s8(x)
 #define vreinterpretq_m128i_s32(x) (x)
-static inline __attribute__((always_inline))
+inline __attribute__((always_inline))
 __m128i _mm_aesenc_si128( __m128i v, __m128i rkey )
 {
     const __attribute__((aligned(16))) __m128i zero = {0};
