c93fd45bbfbd57ea235e8e80ac87afa10e3f5916
jojapoppa <jojapoppa@protonmail.com>
2020-07-25 21:14:54 -0700

    arm...

--- src/Platform/Android/System/TcpConnection.cpp
+++ src/Platform/Android/System/TcpConnection.cpp
@@ -84,10 +84,7 @@ size_t TcpConnection::read(uint8_t* data, size_t size, Logging::LoggerRef &logge
   std::string message;
   ssize_t transferred = ::recv(connection, (void *)data, size, 0);
   if (transferred == -1) {
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wlogical-op"
     if (errno != EAGAIN  && errno != EWOULDBLOCK) {
-#pragma GCC diagnostic pop
       message = "recv failed, " + lastErrorMessage();
     } else {
       epoll_event connectionEvent;
@@ -183,10 +180,7 @@ std::size_t TcpConnection::write(const uint8_t* data, size_t size, Logging::Logg
 
   ssize_t transferred = ::send(connection, (void *)data, size, MSG_NOSIGNAL);
   if (transferred == -1) {
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wlogical-op"
     if (errno != EAGAIN  && errno != EWOULDBLOCK) {
-#pragma GCC diagnostic pop
       message = "send failed, " + lastErrorMessage();
     } else {
       epoll_event connectionEvent;
