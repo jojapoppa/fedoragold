93c5ef2a4b3e787e4d4e585ec0d80df2361cee07
jojapoppa <jojapoppa@protonmail.com>
2020-08-03 16:38:41 -0700

    updated download.sh script

--- download.sh
+++ download.sh
@@ -1,5 +1,5 @@
-wget --no-check-certifiate 5.189.155.88/FEDbu/blockchainindices.dat
-wget --no-check-certifiate 5.189.155.88/FEDbu/blockindexes.dat
-wget --no-check-certifiate 5.189.155.88/FEDbu/blockscache.dat
-wget --no-check-certifiate 5.189.155.88/FEDbu/blocks.dat
-# --no-check-certifiate wget 5.189.155.88/FEDbu/poolstate.bin
+wget --no-check-certifiate 5.189.155.88/FEDbu/blockchainindices.dat --no-check-certifiate
+wget --no-check-certifiate 5.189.155.88/FEDbu/blockindexes.dat --no-check-certifiate
+wget --no-check-certifiate 5.189.155.88/FEDbu/blockscache.dat --no-check-certifiate
+wget --no-check-certifiate 5.189.155.88/FEDbu/blocks.dat --no-check-certifiate
+# --no-check-certifiate wget 5.189.155.88/FEDbu/poolstate.bin --no-check-certifiate
