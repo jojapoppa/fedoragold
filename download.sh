77861ff91560328745d2a999ba5c8f45fa7dc341
jojapoppa <jojapoppa@protonmail.com>
2020-10-20 02:06:44 -0700

    added bootstrap to block explorer server...

--- download.sh
+++ download.sh
@@ -1,5 +1,6 @@
-wget 5.189.155.88/FEDbu/blockchainindices.dat --no-check-certificate
-wget 5.189.155.88/FEDbu/blockindexes.dat --no-check-certificate
-wget 5.189.155.88/FEDbu/blockscache.dat --no-check-certificate
-wget 5.189.155.88/FEDbu/blocks.dat --no-check-certificate
-# --no-check-certifiate wget 5.189.155.88/FEDbu/poolstate.bin --no-check-certificate
+wget explorer.fedoragold.com/bootstrap/blockchainindices.dat --no-check-certificate
+wget explorer.fedoragold.com/bootstrap/blockindexes.dat --no-check-certificate
+wget explorer.fedoragold.com/bootstrap/blockscache.dat --no-check-certificate
+wget explorer.fedoragold.com/bootstrap/blocks.dat --no-check-certificate
+wget explorer.fedoragold.com/bootstrap/p2pstate.bin --no-check-certificate
+wget explorer.fedoragold.com/bootstrap/poolstate.bin --no-check-certificate
