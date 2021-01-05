4f314b2f957744f0e65ba2c88c94f216d9289fc7
Joja Poppa <jojapoppa@protonmail.com>
2021-01-05 11:59:22 -0800

    account for mining...

--- testmine.sh
+++ testmine.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-./build/release/src/fedoragold_daemon --log-level 2 --rpc-bind-ip 0.0.0.0 --start-mining NLr6xycTPnHTEmnsTdoMr93rnejuKxeb8HtDpMyfUkApK4b2cTt4HA4Yhay1HBU1CLgiEhwsM14wCXFriSj7DkFpCy5JMvU --mining-threads 2 --add-priority-node 192.248.151.29:30158 --add-priority-node fed.cryptonote.club:30158
+./build/release/src/fedoragold_daemon --log-level 2 --rpc-bind-ip 0.0.0.0 --start-mining NLFrvaYrRp22UnEtQM6wZRRPsjMwGF6KR46E7xFBeSsD7R3y3NWGKmyHdutk5U1EagGMvD9EBpaKsbzmGmDoTMWw1vLZxJx --mining-threads 4 --add-priority-node 192.248.151.29:30158 --add-priority-node fed.cryptonote.club:30158
 
