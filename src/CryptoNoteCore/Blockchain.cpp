795c8e163eee57aa86dc90a0fb4f6fbb5b4fbfc6
jojapoppa <jojapoppa@protonmail.com>
2021-03-05 10:05:39 -0800

    work on index loader...

--- src/CryptoNoteCore/Blockchain.cpp
+++ src/CryptoNoteCore/Blockchain.cpp
@@ -530,25 +530,34 @@ bool Blockchain::init(const std::string& config_folder, bool load_existing) {
     cachePath = appendPath(config_folder, m_currency.blocksCacheFileName());
     cacheloader.load(cachePath);
 
-    if (cacheloader.loaded()) {
-      if (m_blockchainIndexesEnabled) {
-        loadBlockchainIndices();
-      }
-    } else {
-      logger(INFO) << "Block cache not loaded from: " + cachePath;
-      if (! loadIndexes(config_folder, load_existing)) {
-        logger(INFO) << "Failed to load indexes, resetting indexes file now...";
-        // force resync now
-        remove(blockFilePath.c_str());
-        remove(indexesPath.c_str());
-        if (!m_blocks.open(blockFilePath, indexesPath, 1024)) {
-          logger(ERROR, BRIGHT_RED) << "Failed to open the block file for resync " << blockFilePath << " with indexes path " << indexesPath << " after append of config folder path: " << m_config_folder;
-          return false;
-        }
-
-        loadIndexes(config_folder, false);
-      }
+    if (!cacheloader.loaded()) {
+      logger(INFO) << "No actual blockchain cache found, rebuilding internal structures...";
+      rebuildCache();
     }
+
+    if (m_blockchainIndexesEnabled) {
+      loadBlockchainIndices();
+    }
+
+    //if (cacheloader.loaded()) {
+    //  if (m_blockchainIndexesEnabled) {
+    //    loadBlockchainIndices();
+    //  }
+    //} else {
+    //  logger(INFO) << "Block cache not loaded from: " + cachePath;
+    //  if (! loadIndexes(config_folder, load_existing)) {
+    //    logger(INFO) << "Failed to load indexes, resetting indexes file now...";
+    //    // force resync now
+    //    remove(blockFilePath.c_str());
+    //    remove(indexesPath.c_str());
+    //    if (!m_blocks.open(blockFilePath, indexesPath, 1024)) {
+    //      logger(ERROR, BRIGHT_RED) << "Failed to open the block file for resync " << blockFilePath << " with indexes path " << indexesPath << " after append of config folder path: " << m_config_folder;
+    //      return false;
+    //    }
+    // 
+    //    loadIndexes(config_folder, false);
+    //  }
+    //}
   } else {
     m_blocks.clear();
   }
