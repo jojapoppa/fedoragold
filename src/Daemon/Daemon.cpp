6c5cdd6b33e68c6a044845dd6b5e3a6baa203bce
Joja Poppa <jojapoppa@protonmail.com>
2021-03-11 11:29:01 -0800

    wip

--- src/Daemon/Daemon.cpp
+++ src/Daemon/Daemon.cpp
@@ -26,8 +26,13 @@
 #include "version.h"
 
 #if defined(__APPLE__)
+#define _XOPEN_SOURCE
+#include <stdio.h>
+#include <signal.h>
 #include "execinfo.h"
-#include <dlfcn.h>     // for dladdr
+#include <ucontext.h>
+#include <unistd.h>
+#include <dlfcn.h>
 #endif
 
 #include "Logging/LoggerManager.h"
@@ -134,15 +139,68 @@ std::string backTrace(int skip = 1)
 	return trace_buf.str();
 }
 
-void CrashHandler(int sig) {
-//  void *array[10];
-//  size_t size;
-//  // get void*'s for all entries on the stack
-//  size = backtrace(array, 10);
+typedef ucontext_t sigcontext_t;
+typedef struct _sig_ucontext {
+ unsigned long     uc_flags;
+ ucontext_t        *uc_link;
+ stack_t           uc_stack;
+ sigcontext_t      uc_mcontext;
+ sigset_t          uc_sigmask;
+} sig_ucontext_t;
+void crit_err_hdlr(int sig_num, siginfo_t * info, void * ucontext)
+{
+ void *             array[50];
+ void *             caller_address = NULL;
+ char **            messages;
+ int                size, i;
+ sig_ucontext_t *   uc;
 
-  fprintf(stderr, "Error: signal %d:\n", sig);
-  fprintf(stderr, "Back Trace: \n%s", backTrace().c_str());
-  exit(1);
+ fprintf(stderr, "new crit_err_hdlr");
+ uc = (sig_ucontext_t *)ucontext;
+
+ /* Get the address at the time the signal was raised */
+//#if defined(__i386__) // gcc specific
+// caller_address = (void *) uc->uc_mcontext.eip; // EIP: x86 specific
+//#elif defined(__x86_64__) // gcc specific
+// caller_address = (void *) uc->uc_mcontext.rip; // RIP: x86_64 specific
+//#else
+//#error Unsupported architecture. // TODO: Add support for other arch.
+//#endif
+fprintf(stderr, "signal %d (%s), address is %p from %p\n", 
+  sig_num, strsignal(sig_num), info->si_addr, 
+  (void *)caller_address);
+
+ size = backtrace(array, 50);
+
+ /* overwrite sigaction with caller's address */
+ //array[1] = caller_address;
+
+ messages = backtrace_symbols(array, size);
+
+ /* skip first stack frame (points here) */
+ for (i = 1; i < size && messages != NULL; ++i)
+ {
+  fprintf(stderr, "[bt]: (%d) %s\n", i, messages[i]);
+ }
+
+ free(messages);
+
+ exit(EXIT_FAILURE);
+}
+
+void installSig() {
+ struct sigaction sigact;
+
+ sigact.sa_sigaction = crit_err_hdlr;
+ sigact.sa_flags = SA_RESTART | SA_SIGINFO;
+
+ if (sigaction(SIGSEGV, &sigact, (struct sigaction *)NULL) != 0)
+ {
+  fprintf(stderr, "error setting signal handler for %d (%s)\n",
+    SIGSEGV, strsignal(SIGSEGV));
+
+  exit(EXIT_FAILURE);
+ }
 }
 #endif
 
@@ -154,8 +212,8 @@ int main(int argc, char* argv[])
 #endif
 
 #if defined(__APPLE__)
-  //fprintf(stderr, "** define signal handler on Apple\n");
-  signal(SIGSEGV, CrashHandler);   // install our handler for debug build crashes
+  //signal(SIGSEGV, CrashHandler);   // install our handler for debug build crashes
+  installSig();
 #endif
 
   LoggerManager logManager;
